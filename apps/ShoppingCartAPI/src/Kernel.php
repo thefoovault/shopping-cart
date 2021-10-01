7931d51fae84c349359ba69cac15225f5a2da1db
Thefoovault <marta.ag.ma@gmail.com>
2021-10-01 16:24:09 +0200

    Initial setup

--- apps/ShoppingCartAPI/src/Kernel.php
+++ apps/ShoppingCartAPI/src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SymfonyClient;
+namespace ShoppingCartAPI;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
