237505cd0bc4c726fb097c7688bb0b5ffd32b517
Thefoovault <marta.ag.ma@gmail.com>
2021-10-03 16:57:53 +0200

    Added exception mapping to Add product to cart use case

--- src/ShoppingCart/Application/AddProductToCart/AddProductToCart.php
+++ src/ShoppingCart/Application/AddProductToCart/AddProductToCart.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace ShoppingCart\Application\AddProductToCart;
 
+use ShoppingCart\Domain\Cart\Cart;
 use ShoppingCart\Domain\Cart\CartId;
 use ShoppingCart\Domain\Cart\CartRepository;
+use ShoppingCart\Domain\Cart\Exception\CartNotFound;
 use ShoppingCart\Domain\CartLine\CartLineQuantity;
+use ShoppingCart\Domain\Product\Exception\ProductNotFound;
+use ShoppingCart\Domain\Product\Product;
 use ShoppingCart\Domain\Product\ProductId;
 use ShoppingCart\Domain\Product\ProductRepository;
 
@@ -20,10 +24,27 @@ final class AddProductToCart
     public function __invoke(CartId $cartId, ProductId $productId, CartLineQuantity $cartLineQuantity): void
     {
         $cart = $this->cartRepository->findById($cartId);
+        $this->assertCartExists($cart, $cartId);
+
         $product = $this->productRepository->findById($productId);
+        $this->assertProductExists($product, $productId);
 
         $cart->addProduct($product, $cartLineQuantity);
 
         $this->cartRepository->save($cart);
+    }
+
+    private function assertCartExists(?Cart $cart, CartId $cartId): void
+    {
+        if (null === $cart) {
+            throw new CartNotFound($cartId);
+        }
+    }
+
+    private function assertProductExists(?Product $product, ProductId $productId): void
+    {
+        if (null === $product) {
+            throw new ProductNotFound($productId);
+        }
     }
 }
